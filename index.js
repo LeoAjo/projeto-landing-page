ca470bf946211fa1e991c2bb3d256267ab077c58
LeoAjo <leandrosilvaraujo471@gmail.com>
2025-06-22 19:10:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var Leonardo = window.document.getElementById("Leonardo"); 
-var Bruna = window.document.getElementById("Bruna");
 var Samantha = window.document.getElementById("Samantha");
+var Bruna = window.document.getElementById("Bruna");
 var setaEsquerda = window.document.getElementById("setaEsquerda"); 
 var setaDireita = window.document.getElementById("setaDireita");
 
@@ -8,16 +8,16 @@ var setaDireita = window.document.getElementById("setaDireita");
   function RolarParaEsquerda() {
    
         Leonardo.style.display = "none";
-        Bruna.style.display = "block";
         Samantha.style.display = "block";
+        Bruna.style.display = "block";
         setaEsquerda.style.display = "none";
         setaDireita.style.display = "block";
     
 }
 function RolarParaDireita() {
-    Leonardo.style.display = "none";
-    Bruna.style.display = "block";
-    Samantha.style.display ="block"
+    Leonardo.style.display = "block";
+    Samantha.style.display ="block";
+    Bruna.style.display = "none";
     setaEsquerda.style.display = "block";
     setaDireita.style.display = "none"; 
 }
