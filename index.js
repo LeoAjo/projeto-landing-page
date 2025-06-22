8822016d7ae68add15bc34b6ea952a0d1578ea89
LeoAjo <leandrosilvaraujo471@gmail.com>
2025-06-22 18:41:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,13 @@ var setaDireita = window.document.getElementById("setaDireita");
 
 
   function RolarParaEsquerda() {
-    if (Leonardo && Bruna && Samantha && setaEsquerda && setaDireita) {
+   
         Leonardo.style.display = "none";
         Bruna.style.display = "block";
         Samantha.style.display = "block";
         setaEsquerda.style.display = "none";
         setaDireita.style.display = "block";
-    } else {
-        console.error("Erro: Um ou mais elementos não foram encontrados.");
-    }
+    
 }
 function RolarParaDireita() {
     Leonardo.style.display = "none";
